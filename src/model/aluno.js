9c891337afb1d916c6c8091df3df5a59c58892a0
GabrielInacio07 <127869492+GabrielInacio07@users.noreply.github.com>
2025-04-17 22:20:00 -0300

    add model de aluno

---
+++ src/model/aluno.js
@@ -0,0 +1,8 @@
+import { User } from './user.js'
+
+export class Aluno extends User {
+  constructor(nome, email, telefone, cpf, senha, faculdade) {
+    super(nome, email, telefone, cpf, senha)
+    this.faculdade = faculdade
+  }
+}
